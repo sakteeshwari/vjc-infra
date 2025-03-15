719aafc754a1eaa2541e9a8a75acd2a642357534
sakteeshwari <sakthijessy26@gmail.com>
2025-03-15 19:51:07 +0530

    first commit

---
+++ app/.eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+    // your existing configuration
+    rules: {
+      "react/no-unescaped-entities": "off", // disables the rule
+      "no-unused-vars": "warn", // sets unused vars to a warning
+      // Add any other rules you want to disable or adjust here
+    },
+  };
+  
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Navbar from "./components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <div className="font-montserrat">
+      <Navbar />
+      {/* Main Content */}
+    </div>
+  );
+}

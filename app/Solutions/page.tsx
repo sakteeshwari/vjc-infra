2d161c46c5378a576872a49a8cb4f069d5cc3d03
sakteeshwari <sakthijessy26@gmail.com>
2025-03-21 15:44:54 +0530

    first commit

--- app/Solutions/page.tsx
+++ app/Solutions/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
-import Solutionimg from '../components/Solutionimg/Solutionimg'
+// import Solutionimg from '../components/Solutionimg/Solutionimg'
 
 
 const page = () => {
   return (
     <div>
-        <Solutionimg></Solutionimg>
+        {/* <Solutionimg></Solutionimg> */}
       <Footer></Footer>
     </div>
   )
